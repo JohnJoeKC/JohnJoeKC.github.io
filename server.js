a25fd6acb7618f823010f1d630fddbd2fbbe2dc7
JJ <jjcasey4@hotmail.com>
2023-04-12 18:39:46 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const Docker = require('dockerode');
+const app = express();
+const cors = require('cors');
+const docker = new Docker({ host: 'localhost', port: 2375 });
+
+app.use(express.static('public')); // Add this line to serve static files from the "public" folder
+app.use(cors({
+  origin: 'http://127.0.0.1:5500',
+  methods: ['POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type']
+}));
+app.use(express.json()); // Add this line to parse JSON from incoming requests
+
+app.post('/execute', async (req, res) => {
+    try {
+      // Extract code and language from the request (assumes JSON format)
+      const { code, language } = req.body;
+  
+      if (language !== 'python') {
+        res.status(400).json({ error: 'Unsupported language' });
+        return;
+      }
+  
+      // Create an isolated Docker container to run the code
+      const container = await docker.createContainer({
+        Image: 'python:3.8', // Use the official Python 3.8 image
+        Cmd: ['python', '-c', code], // Run the provided code
+        Tty: false,
+        AttachStdout: true,
+        AttachStderr: true,
+      });
+  
+      // Start the container
+      await container.start();
+  
+      // Attach a stream to collect the output
+      const stream = await container.attach({ stdout: true, stderr: true, stream: true });
+      let output = '';
+  
+      // Listen for data events and collect the output
+      stream.on('data', (chunk) => {
+        output += chunk.toString('utf-8');
+      });
+  
+      // Wait for the container to finish executing the code
+      await container.wait();
+  
+      // Clean up the container
+      await container.remove();
+  
+      // Send the output back to the client
+      res.json({ output });
+    } catch (error) {
+      console.error('Error details:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  
+});
+  
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
